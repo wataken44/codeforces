e755c5e8128f7c48f6c8495d8405056135839884
Kenichi Watanabe <wataken44@maria.local>
2013-06-10 22:44:13 +0900

    add: 304C, 312ACE

---
+++ problems/304/304E.cpp
@@ -0,0 +1,128 @@
+/*
+  304E
+ */
+#include <algorithm>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+typedef int int_type;
+
+// -- utility --
+// C-style loop
+#define FOR(x, a, b) for(int_type x = static_cast<int_type>(a); x < static_cast<int_type>(b); ++x)
+#define FORN(x, n) FOR(x, 0, n)
+// Ruby-style loop
+#define TIMES(x, n) FOR(x, 0, n)
+#define STEP(x, a, b, s) for(int_type x = static_cast<int_type>(a); s > 0 ? x <= static_cast<int_type>(b) : x >= static_cast<int_type>(b); x += static_cast<int_type>(s) )
+#define UPTO(x, a, b) for(int_type x = static_cast<int_type>(a); x <= static_cast<int_type>(b); ++x)
+#define DOWNTO(x, a, b) for(int_type x = static_cast<int_type>(a); x >= static_cast<int_type>(b); --x)
+#define EACH(c, i) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define ALL(cont, it, cond, ret) \
+  bool ret = true; EACH(cont, it) { if(!(cond)) {ret=false;break;} }
+#define ANY(cont, it, cond, ret) \
+  bool ret = false; EACH(cont, it) { if(cond) {ret=true;break;} }
+
+// debug
+#define DUMP(x) std::cout << #x << " = " << to_s(x) << std::endl;
+
+using namespace std;
+
+template<typename T> string to_s(const T& v);
+template<> string to_s(const string& v);
+template<> string to_s(const bool& v);
+template<typename T> string to_s(const vector<T>& v);
+template<typename T> string to_s(const list<T>& v);
+template<typename T> string to_s(const set<T>& v);
+template<typename F, typename S> string to_s(const pair<F,S>& v);
+template<typename K, typename V> string to_s(const map<K,V>& v);
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*
+  304E
+ */
+
+int main(int argc, char *argv[])
+{
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> a;
+  TIMES(nn, n) {
+    int aa;
+    cin >> aa;
+    a.push_back(aa);
+  }
+  
+  if(n - k <= 1) {
+    cout << 1 << endl;
+    return 0;
+  }
+
+  vector<int> d(1000002, 0);
+  FOR(k, 0, n) {
+    FOR(j, k+1, n) {
+      int dd = abs(a[k] - a[j]);
+      ++d[dd];
+    }
+  }
+  
+  ll m = n - k - 1;
+  while(true) {
+    ++m;
+
+    int p = 0;
+    STEP(x, m, 1000001, m) {
+      p += d[x]; 
+    }
+    if(p > k * (k + 1) / 2) continue;
+    
+    vector<int> s(n);
+    TIMES(nn, n) {
+      int mod = a[nn] % m;
+      s[nn] = mod;
+    }
+
+    sort(s.begin(), s.end());
+
+    int over = 0;
+    UPTO(nn, 1, n - 1) {
+      if(s[nn] == s[nn - 1]) over += 1;
+    }
+    
+    if(over <= k) {
+      cout << m << endl;
+      break;
+    }
+  }
+  
+  return 0;
+}
+
+
+template<typename T> string to_s(const T& v) { ostringstream oss; oss << v; return oss.str(); }
+template<> string to_s(const string& v) { ostringstream oss; oss << '"' << v << '"'; return oss.str(); }
+template<> string to_s(const bool& v) { ostringstream oss; oss << ( v ? "true" : "false") ; return oss.str(); } 
+template<typename T> string to_s(const vector<T>& v) { ostringstream oss; oss << "["; EACH(v,i) oss << to_s(*i) << ","; oss << "]"; return oss.str(); }
+template<typename T> string to_s(const list<T>& v) { ostringstream oss; oss << "("; EACH(v,i) oss << to_s(*i) << ","; oss << ")"; return oss.str(); }
+template<typename T> string to_s(const set<T>& v) { ostringstream oss; oss << "{"; EACH(v,i) oss << to_s(*i) << ","; oss << "}"; return oss.str(); }
+template<typename F, typename S> string to_s(const pair<F,S>& v) { ostringstream oss; oss << "<" << to_s(v.first) << " " << to_s(v.second) << ">"; return oss.str(); }
+template<typename K, typename V> string to_s(const map<K,V>& v) { ostringstream oss; oss << "{"; EACH(v,i) oss << to_s(i->first) << " => " << to_s(i->second) << ","; oss << "}"; return oss.str(); }
