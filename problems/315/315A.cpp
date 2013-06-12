fe1137b1db3429f8c44e4376128743eef014149f
Kenichi Watanabe <wataken44@maria.local>
2013-06-12 21:11:11 +0900

    add: 315AB

---
+++ problems/315/315A.cpp
@@ -0,0 +1,105 @@
+/*
+  315A
+ */
+#include <algorithm>
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+typedef int int_type;
+
+// -- utility --
+// C-style loop
+#define FOR(x, a, b) for(int_type x = static_cast<int_type>(a); x < static_cast<int_type>(b); ++x)
+#define FORN(x, n) FOR(x, 0, n)
+// Ruby-style loop
+#define TIMES(x, n) FOR(x, 0, n)
+#define STEP(x, a, b, s) for(int_type x = static_cast<int_type>(a); s > 0 ? x <= static_cast<int_type>(b) : x >= static_cast<int_type>(b); x += static_cast<int_type>(s) )
+#define UPTO(x, a, b) for(int_type x = static_cast<int_type>(a); x <= static_cast<int_type>(b); ++x)
+#define DOWNTO(x, a, b) for(int_type x = static_cast<int_type>(a); x >= static_cast<int_type>(b); --x)
+#define EACH(c, i) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
+#define ALL(cont, it, cond, ret) \
+  bool ret = true; EACH(cont, it) { if(!(cond)) {ret=false;break;} }
+#define ANY(cont, it, cond, ret) \
+  bool ret = false; EACH(cont, it) { if(cond) {ret=true;break;} }
+
+// debug
+#define DUMP(x) std::cout << #x << " = " << to_s(x) << std::endl;
+
+using namespace std;
+
+template<typename T> string to_s(const T& v);
+template<> string to_s(const string& v);
+template<> string to_s(const bool& v);
+template<typename T> string to_s(const vector<T>& v);
+template<typename T> string to_s(const list<T>& v);
+template<typename T> string to_s(const set<T>& v);
+template<typename F, typename S> string to_s(const pair<F,S>& v);
+template<typename K, typename V> string to_s(const map<K,V>& v);
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*
+  315A
+ */
+
+int main(int argc, char *argv[])
+{
+  int n;
+  cin >> n;
+
+  vector<int> a;
+  vector<int> b;
+  vector<bool> o;
+  
+  TIMES(nn, n) {
+    int aa, bb;
+    cin >> aa >> bb;
+    a.push_back(aa);
+    b.push_back(bb);
+    o.push_back(false);
+  }
+
+  TIMES(i, n) {
+    TIMES(j, n) {
+      if(i == j) continue;
+      if(b[i] == a[j]) o[j] = true;
+    }
+  }
+
+  int r = 0;
+  TIMES(i, n) {
+    if(o[i] == false) r += 1;
+  }
+  
+  cout << r << endl;
+  
+  return 0;
+}
+
+
+template<typename T> string to_s(const T& v) { ostringstream oss; oss << v; return oss.str(); }
+template<> string to_s(const string& v) { ostringstream oss; oss << '"' << v << '"'; return oss.str(); }
+template<> string to_s(const bool& v) { ostringstream oss; oss << ( v ? "true" : "false") ; return oss.str(); } 
+template<typename T> string to_s(const vector<T>& v) { ostringstream oss; oss << "["; EACH(v,i) oss << to_s(*i) << ","; oss << "]"; return oss.str(); }
+template<typename T> string to_s(const list<T>& v) { ostringstream oss; oss << "("; EACH(v,i) oss << to_s(*i) << ","; oss << ")"; return oss.str(); }
+template<typename T> string to_s(const set<T>& v) { ostringstream oss; oss << "{"; EACH(v,i) oss << to_s(*i) << ","; oss << "}"; return oss.str(); }
+template<typename F, typename S> string to_s(const pair<F,S>& v) { ostringstream oss; oss << "<" << to_s(v.first) << " " << to_s(v.second) << ">"; return oss.str(); }
+template<typename K, typename V> string to_s(const map<K,V>& v) { ostringstream oss; oss << "{"; EACH(v,i) oss << to_s(i->first) << " => " << to_s(i->second) << ","; oss << "}"; return oss.str(); }
